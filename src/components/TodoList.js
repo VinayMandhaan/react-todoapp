51bc9eb421b605d241241b7c35a777a36aef2f96
VinayMandhaan <vinaymandhaan29@gmail.com>
2019-08-02 16:40:25 +0500

    project

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import TodoItem from "./TodoItem"
+
+export default class TodoList extends Component {
+    render() {
+        const{items,clearList,handleDelete,handleEdit} = this.props
+        return (
+            <ul className="list-group my-5">
+                <h3 className="text capitalize text-center">Todo List</h3>
+                {
+                    items.map(item=>{
+                        let a = item.title !== "" ?<TodoItem key={item.id} title={item.title} 
+                        handleDelete={()=> handleDelete(item.id)}handleEdit={()=> handleEdit(item.id)}/> : null
+                        return <div>
+                            {a}
+                        </div>
+                    })
+                }
+                
+
+                <button className="btn btn-block btn-danger mt-5" onClick={clearList} type="button">Clear</button>
+            </ul>
+        )
+    }
+}
